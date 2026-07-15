77f91898a365be797825f274d55c44adcc7ca595
sharathgowdaur-jpg <sharathgowdaur@gmail.com>
2026-07-15 08:36:23 +0530

    Add leetcode - 3658. GCD of Odd and Even Sums

---
+++ Python3/Easy/3658. GCD of Odd and Even Sums.py
@@ -0,0 +1,17 @@
+# SOLUTION - 1
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        sumOdd=0
+        sumEven=0
+        for i in range(1,(n*2)+1):
+            if i%2==0:
+                sumEven+=i
+            else:
+                sumOdd+=i
+        return math.gcd(sumOdd,sumEven)
+
+
+    #SOLUTION - 2
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        return n
